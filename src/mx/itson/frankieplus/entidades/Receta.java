eff94265576b9f9914342d7d1d2c87515706da09
jesus <jesus@192.168.1.64>
2022-10-20 01:42:15 -0700

    Proyecto Examen

---
+++ src/mx/itson/frankieplus/entidades/Receta.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.frankieplus.entidades;
+
+import com.google.gson.Gson;
+import java.util.List;
+import mx.itson.frankieplus.enumeradores.Dificultad;
+
+/**
+ *
+ * @author jesus
+ */
+public class Receta {
+    
+    private String nombre;
+    private String descripcion;
+    private int numeroPorciones;
+    private int duracion;
+    private List<Ingrediente> ingredientes;
+    private List<Paso> pasos;
+    private Dificultad dificultad;
+    private Usuario usuario;
+    
+    public Receta deserializar(String json) {
+        
+        Receta receta = new Receta();
+        try {
+            receta = new Gson().fromJson(json, Receta.class);
+        } catch(Exception ex){
+            System.err.print("Ocurrió un error: " +ex.getMessage());
+        }
+        return receta;
+    } 
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the numeroPorciones
+     */
+    public int getNumeroPorciones() {
+        return numeroPorciones;
+    }
+
+    /**
+     * @param numeroPorciones the numeroPorciones to set
+     */
+    public void setNumeroPorciones(int numeroPorciones) {
+        this.numeroPorciones = numeroPorciones;
+    }
+
+    /**
+     * @return the duracion
+     */
+    public int getDuracion() {
+        return duracion;
+    }
+
+    /**
+     * @param duracion the duracion to set
+     */
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    /**
+     * @return the ingredientes
+     */
+    public List<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    /**
+     * @param ingredientes the ingredientes to set
+     */
+    public void setIngredientes(List<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    /**
+     * @return the paso
+     */
+    public List<Paso> getPasos() {
+        return pasos;
+    }
+
+    /**
+     * @param paso the paso to set
+     */
+    public void setPasos(List<Paso> paso) {
+        this.pasos = paso;
+    }
+
+    /**
+     * @return the dificultad
+     */
+    public Dificultad getDificultad() {
+        return dificultad;
+    }
+
+    /**
+     * @param dificultad the dificultad to set
+     */
+    public void setDificultad(Dificultad dificultad) {
+        this.dificultad = dificultad;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+}
---
+++ src/mx/itson/frankieplus/entidades/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.frankieplus.entidades;
+
+/**
+ *
+ * @author jesus
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String email;
+    private String password;
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
